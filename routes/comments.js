7c973867242a2240eb421a3a7dae83ffd82ac218
Benoît Hubert <benoithubert@gmail.com>
2019-12-13 01:48:48 +0100

    Filter the comments using the postId param

--- routes/comments.js
+++ routes/comments.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const fakeComments = require('../data/comments');
 
-const router = express.Router();
+const router = express.Router({ mergeParams: true });
 
 // Get a list of comments
 router.get('/', (req, res) => {
-  res.json(fakeComments);
+  // If we forget { mergeParams: true }, req.params.postId will be `undefined`
+  const postId = Number(req.params.postId);
+  // Keep only comments whose post_id matches the postId parameter
+  const postComments = fakeComments.filter((comment) => comment.post_id === postId);
+  res.json(postComments);
 });
 
 module.exports = router;
